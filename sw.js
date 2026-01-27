f3532a9f2139123d0970ca3642e25e0d897dab1a
Sagar Herole <129059947+sagar7869@users.noreply.github.com>
2026-01-27 11:42:24 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installing.');
-});
-self.addEventListener('fetch', function(event) {
-  // Optional: cache handling for offline
-});
